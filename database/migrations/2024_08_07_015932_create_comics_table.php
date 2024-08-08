2bb85ea4a76b04506c5ee6b5650cf01cee609e1a
Awogaaa <perdanak799@gmail.com>
2024-08-08 09:51:47 +0700

    migrati

--- database/migrations/2024_08_07_015932_create_comics_table.php
+++ database/migrations/2024_08_07_015932_create_comics_table.php
@@ -15,19 +15,10 @@ class CreateComicsTable extends Migration
             $table->string('image')->nullable(); // Adding image column
             $table->timestamps();
         });
-
-        // Create the pivot table for comics and genres
-        Schema::create('comic_genre', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('comic_id')->constrained()->onDelete('cascade');
-            $table->foreignId('genre_id')->constrained()->onDelete('cascade');
-            $table->timestamps();
-        });
     }
 
     public function down()
     {
-        Schema::dropIfExists('comic_genre');
         Schema::dropIfExists('comics');
     }
 }
