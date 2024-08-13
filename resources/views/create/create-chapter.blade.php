7d89bf1b14ce7842e7c03d6770aba2f975dcbb3b
Awogaaa <perdanak799@gmail.com>
2024-08-13 09:47:55 +0700

    Update create-chapter.blade.php

--- resources/views/create/create-chapter.blade.php
+++ resources/views/create/create-chapter.blade.php
@@ -7,10 +7,6 @@
             <input type="hidden" name="comic_id" value="{{ request()->query('comic_id') }}" />
 
             <div class="mb-4">
-                <label for="number" class="block text-sm font-medium text-gray-700">Number</label>
-                <input type="number" name="number" id="number" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required>
-            </div>
-            <div class="mb-4">
                 <label for="images" class="block text-sm font-medium text-gray-700">Images</label>
                 <div id="image-inputs">
                     <input type="file" name="images[]" id="images" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" multiple>
