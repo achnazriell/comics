ace256d00de15032cfb0bd54157469d38b5ed52b
Awogaaa <perdanak799@gmail.com>
2024-08-19 09:17:56 +0700

    jnoiajdoisja

--- resources/views/update/edit-chapter.blade.php
+++ resources/views/update/edit-chapter.blade.php
@@ -16,13 +16,12 @@
                 </div>
 
                 <!-- Menampilkan gambar yang sudah ada -->
-                <div id="currentChapterSection">
+                <div class="mb-4">
                     <h3 class="text-sm font-medium text-gray-700 mb-2">Current Chapter Images</h3>
                     <div class="grid grid-cols-3 gap-4 mb-4">
                         @foreach($chapter->images as $image)
                             <div class="relative">
                                 <img src="{{ asset('chapter_images/' . $image->image) }}" alt="Chapter Image" class="w-full h-32 object-cover">
-                                <!-- Checkbox untuk menghapus gambar -->
                                 <input type="checkbox" name="delete_images[]" value="{{ $image->id }}" class="absolute top-0 right-0 m-2">
                             </div>
                         @endforeach
@@ -33,10 +32,8 @@
                 <div id="chapterSection">
                     <h3 class="text-sm font-medium text-gray-700 mb-2"></h3>
                     <div class="mb-4">
-                        <label for="chapter_images" class="block text-sm font-medium text-gray-700">Chapter Images</label>
-                        <input type="file" name="chapter_images[]" id="chapter_images"
-                            class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                            multiple>
+                        <label for="chapter_images_{{ $chapter->id }}" class="block text-sm font-medium text-gray-700">New Chapter Images</label>
+                        <input type="file" name="chapter_images[]" id="chapter_images_{{ $chapter->id }}" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" multiple>
                     </div>
                 </div>
 
@@ -45,10 +42,9 @@
                     class="mt-2 bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600">Add Another Chapter Image</button>
 
                 <!-- Tombol navigasi -->
-                <button type="button" class="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600"
-                    onclick="showStep(2)">Back</button>
-                <button type="submit"
-                    class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Finish</button>
+                <div class="mb-4">
+                    <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">Update Chapter {{ $chapter->number }}</button>
+                </div>
             </form>
         </div>
     </div>
